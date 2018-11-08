3cadbd609ecee3671f750ee1f47d44b8ac42f97b
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-11-08 22:51:12 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,11 +9,11 @@ class Post < ActiveRecord::Base
   end
 
   def self.from_today
-    where("created_at >=?", Time.zone.today.beginning_of_today)
+    where("created_at >=?", Time.zone.today.beginning_of_day)
   end
 
   def self.old_news
-    where("created_at <=?", Time.zone.today.beginning_of_today)
+    where("created_at <=?", Time.zone.today.beginning_of_day)
   end
 
   private
