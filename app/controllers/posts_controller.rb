8a23fcf305b16e983d6bd00321a98e2f44675be4
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-11-08 22:40:29 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,9 @@ class PostsController < ApplicationController
     if !params[:author].blank?
       @posts = Post.where(author: params[:author])
     elsif !params[:date].blank?
-
+      if params[:date] == "Today"
+      else
+      end
     else
       @posts = Post.all
     end
